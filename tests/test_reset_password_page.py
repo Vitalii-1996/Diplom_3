74232923ae929c8a984eb18188a5668fd6ec614b
Vitalii-1996 <vitalii.nikolaichuk1996@gmail.com>
2025-07-28 21:35:47 +0300

    add reset password locators, page and tests

---
+++ tests/test_reset_password_page.py
@@ -0,0 +1,23 @@
+from pages.reset_password_page import ResetPasswordPage
+from helpers import generate_random_email
+import pytest
+import data
+
+
+class TestResetPasswordPage:
+    @pytest.mark.parametrize("driver", ["Chrome", "Firefox"], indirect=True)
+    def test_reset_password_success_flow(self, driver):
+        reset_password_page = ResetPasswordPage(driver)
+        reset_password_page.go_to_url(data.RESET_PASSWORD_URL)
+        random_email = generate_random_email()
+        reset_password_page.set_email(random_email)
+        reset_password_page.click_reset_password_button()
+
+        assert reset_password_page.check_reset_password_label_present()
+
+    @pytest.mark.parametrize("driver", ["Chrome", "Firefox"], indirect=True)
+    def test_click_show_password(self, reset_password):
+        login_page = ResetPasswordPage(reset_password)
+        login_page.click_show_password()
+        assert login_page.check_password_field_active()
+          
